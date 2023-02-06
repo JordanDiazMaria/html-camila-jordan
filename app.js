6551c7dda9ed2c154cb2e791e3fc097ca0a79c54
Jordan <123569630+JordanDiazMaria@users.noreply.github.com>
2023-02-06 12:05:21 -0500

    6/02/2023

---
+++ app.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardsAdj = [
+    {
+      name: "img1",
+      imagenes: "imagenes/img1.jpg"
+    },
+    {
+      name: "img2",
+      imagenes: "imagenes/img2.jpg"
+    },
+    {
+      name: "img3",
+      imagenes: "imagenes/img3.jpg"
+    },
+    {
+      name: "img4",
+      imagenes: "imagenes/img4.jpg"
+    },
+    {
+      name: "img5",
+      imagenes: "imagenes/img5.jpg"
+    },
+    {
+      name: "img6",
+      imagenes: "imagenes/img6.jpg"
+    },
+    {
+      name: "img1",
+      imagenes: "imagenes/img1.jpg"
+    },
+    {
+      name: "img2",
+      imagenes: "imagenes/img2.jpg"
+    },
+    {
+      name: "img3",
+      imagenes: "imagenes/img3.jpg"
+    },
+    {
+      name: "img4",
+      imagenes: "imagenes/img4.jpg"
+    },
+    {
+      name: "img5",
+      imagenes: "imagenes/img5.jpg"
+    },
+    {
+      name: "img6",
+      imagenes: "imagenes/img6.jpg"
+    }
+  ];
+});
