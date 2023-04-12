0cb3e6e9e8cb5183ca741f9ecd7633a5ae3e817a
Jordan <123569630+JordanDiazMaria@users.noreply.github.com>
2023-04-12 10:35:29 -0500

    cod

--- app.js
+++ app.js
@@ -2,68 +2,107 @@ document.addEventListener("DOMContentLoaded", () => {
   const cardAdj = [
     {
       name: "img1",
-      imagenes: "imagenes/img1.jpg"
+      img: "imagenes/img1.png"
     },
     {
       name: "img2",
-      imagenes: "imagenes/img2.jpg"
+      img: "imagenes/img2.png"
     },
     {
       name: "img3",
-      imagenes: "imagenes/img3.jpg"
+      img: "imagenes/img3.png"
     },
     {
       name: "img4",
-      imagenes: "imagenes/img4.jpg"
+      img: "imagenes/img4.png"
     },
     {
       name: "img5",
-      imagenes: "imagenes/img5.jpg"
+      img: "imagenes/img5.png"
     },
     {
       name: "img6",
-      imagenes: "imagenes/img6.jpg"
+      img: "imagenes/img6.png"
     },
     {
       name: "img1",
-      imagenes: "imagenes/img1.jpg"
+      img: "imagenes/img1.png"
     },
     {
       name: "img2",
-      imagenes: "imagenes/img2.jpg"
+      img: "imagenes/img2.png"
     },
     {
       name: "img3",
-      imagenes: "imagenes/img3.jpg"
+      img: "imagenes/img3.png"
     },
     {
       name: "img4",
-      imagenes: "imagenes/img4.jpg"
+      img: "imagenes/img4.png"
     },
     {
       name: "img5",
-      imagenes: "imagenes/img5.jpg"
+      img: "imagenes/img5.png"
     },
     {
       name: "img6",
-      imagenes: "imagenes/img6.jpg"
+      img: "imagenes/img6.png"
     }
   ];
-});
-
 
   const cuadricula = document.querySelector(".cuadricula");
   const resultado = document.querySelector("#resultado");
   var cartasEscogidas = [];
+  var cartasEscogidasId = [];
   var cartasGanadas = [];
-  ///____________________________
+
   function crearTablero() {
     for (let i = 0; i < cardAdj.length; i++) {
       var carta = document.createElement("img");
-      carta.setAttribute("src", "images/reverso.png");
+      carta.setAttribute("src", "imagenes/reverso.png");
       carta.setAttribute("data-id", i);
-      carta.addEventListener("click", VoltearCarta);
+      carta.addEventListener("click", voltearCarta);
       cuadricula.appendChild(carta);
     }
   }
+  function verificarPareja() {
+    var cards = document.querySelectorAll("img");
+    const opcionUnoId = cartasEscogidasId[0];
+    const opcionDosId = cartasEscogidasId[1];
+
+    if (opcionUnoId === opcionDosId) {
+      cards[opcionUnoId].setAttribute("src", "imagenes/reverso.png");
+      cards[opcionDosId].setAttribute("src", "imagenes/reverso.png");
+      alert("¡Diste click a la misma imagen! :(");
+    } else if (cartasEscogidas[0] === cartasEscogidas[1]) {
+      alert("¡Correcto :D!");
+      cards[opcionUnoId].setAttribute("src", "imagenes/blank.png");
+      cards[opcionDosId].setAttribute("src", "imagenes/blank.png");
+      cards[opcionUnoId].removeEventListener("click", voltearCarta);
+      cards[opcionDosId].removeEventListener("click", voltearCarta);
+      cartasGanadas.push(cartasEscogidas);
+    } else {
+      cards[opcionUnoId].setAttribute("src", "imagenes/reverso.png");
+      cards[opcionDosId].setAttribute("src", "imagenes/reverso.png");
+      alert("¡Intenta de nuevo!");
+    }
+    cartasEscogidas = [];
+    cartasEscogidasId = [];
+
+    resultado.textContent = cartasGanadas.length;
+
+    if (cartasGanadas.length === cardAdj.length / 2) {
+      resultado.textContent = "¡Felicidades, encontraste todos los pares!";
+    }
+  }
+  function voltearCarta() {
+    var cardId = this.getAttribute("data-id");
+    cartasEscogidas.push(cardAdj[cardId].name);
+    cartasEscogidasId.push(cardId);
+    this.setAttribute("src", cardAdj[cardId].img);
+    if (cartasEscogidas.length === 2) {
+      setTimeout(verificarPareja, 1500);
+    }
+  }
+  crearTablero();
 });
